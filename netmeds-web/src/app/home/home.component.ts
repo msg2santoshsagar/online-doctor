edd2c6cb34da33f1adf92189ddaf3a4449e18f9c
Santosh Sagar <santosh.sagar@tcs.com>
2018-10-20 17:08:36 +0530

    Layouts added

--- netmeds-web/src/app/home/home.component.ts
+++ netmeds-web/src/app/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  doctorList: any = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+  doctorList1: any = [1, 2];
+
   constructor() { }
 
   ngOnInit() {
